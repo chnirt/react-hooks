5c7939a4292fdf314388dd40fd4646306706db72
chin trinh <trinhchinchin@gmail.com>
2020-08-31 16:38:52 +0700

    Todo circleci

--- test/useCounter.test.ts
+++
@@ -1,8 +0,0 @@
-import { shallow } from 'enzyme'
-import { render, screen, fireEvent } from '@testing-library/react'
-
-import { DOMCounter } from '../src'
-
-describe('<DOMCounter />', () => {
-	test('renders the component', () => {})
-})
